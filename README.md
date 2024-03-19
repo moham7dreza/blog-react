ffa9ba053566ca8cf9a42c180acdbe1fb6bf2979
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-19 17:20:18 +0330

    Add : work with react from start to css modules

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Course important notes
+
+- components must be pure -> states and props could not be changed directly in child components
+- pure components -> when component is received props, act something with them and return them without changing them
+  directly
+- change one of states in class components not affect in other states but in function components we must rewrite all
+  states when changing one of states
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from "./Components/Counter.jsx";
+import TestClass from "./Components/TestClass.jsx";
+import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    const [count, setCount] = useState(0)
+    const increaseCount = () => {
+        setCount(count + 1)
+    }
+    const decreaseCount = () => {
+        if (count < 1) {
+            alert('increase count')
+            return
+        }
+        setCount(count - 1)
+    }
+    const resetCount = () => {
+        setCount(0)
+    }
+    return (
+        <>
+            <Counter count={count} increaseCount={increaseCount} decreaseCount={decreaseCount} resetCount={resetCount}/>
+            <TestClass count={undefined}/>
+        </>
+    );
 }
 
 export default App
