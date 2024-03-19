b2175e4992205cb40cf1ee52eba1c3c0a1426547
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-20 01:17:34 +0330

    add hero form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Header} from "./Components/Layouts/Header.jsx";
 import {Footer} from "./Components/Layouts/Footer.jsx";
 import {Stats} from "./Components/Stats.jsx";
 import {Gallery} from "./Components/Gallery.jsx";
+import {HeroForm} from "./Components/HeroForm.jsx";
 
 const App = () => {
     const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ const App = () => {
 
             {/*// <!-- ========== MAIN CONTENT ========== -->*/}
             <main id="content" role="main">
+                <HeroForm/>
                 <Gallery/>
                 <Stats/>
             </main>
