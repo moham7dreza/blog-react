2c20a2fb6c8e382a2f47228eef32f710914459a5
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-20 00:50:12 +0330

    add footer

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -113,7 +113,7 @@ export const Header = () => {
             {/*</header>*/}
 
             <header
-                className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white text-sm py-3 md:py-0 dark:bg-slate-800 rounded-2xl">
+                className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white text-sm py-3 md:py-0 dark:bg-slate-900 rounded-2xl">
                 <nav className="max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8" aria-label="Global">
                     <div className="relative md:flex md:items-center md:justify-between">
                         <div className="flex items-center justify-between">
